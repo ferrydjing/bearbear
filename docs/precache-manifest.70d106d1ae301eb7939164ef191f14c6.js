d9a1cb7f6da7d843044eafa93a1c329aabeddeab
ferrydjing <ferrydjing@outlook.com>
2020-09-17 11:38:05 +0800

    update docs

---
+++ docs/precache-manifest.70d106d1ae301eb7939164ef191f14c6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748f663a04c0cf4d1821d4476bc60abf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "512e602268a82e321fa9",
+    "url": "./static/css/0.f8e1354c.chunk.css"
+  },
+  {
+    "revision": "295644cb4427dff51ec3",
+    "url": "./static/css/10.81cd42c2.chunk.css"
+  },
+  {
+    "revision": "846f3890a10a93a801e6",
+    "url": "./static/css/9.4f473478.chunk.css"
+  },
+  {
+    "revision": "f9731e460608881d8954",
+    "url": "./static/css/table-pages.1eee58f8.chunk.css"
+  },
+  {
+    "revision": "512e602268a82e321fa9",
+    "url": "./static/js/0.bc95c1fb.chunk.js"
+  },
+  {
+    "revision": "295644cb4427dff51ec3",
+    "url": "./static/js/10.18c9e5cc.chunk.js"
+  },
+  {
+    "revision": "19e867a70d97e2a45c7d",
+    "url": "./static/js/11.42a8b7e5.chunk.js"
+  },
+  {
+    "revision": "997b2f1e6a7e55b7402c",
+    "url": "./static/js/404.d7a92d7b.chunk.js"
+  },
+  {
+    "revision": "846f3890a10a93a801e6",
+    "url": "./static/js/9.60309cb7.chunk.js"
+  },
+  {
+    "revision": "1842d9cb69adf43a404dd14443e8ca5f",
+    "url": "./static/js/9.60309cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cdee42de84332e7d307",
+    "url": "./static/js/form-pages.49b701e3.chunk.js"
+  },
+  {
+    "revision": "3e2d2e5b3f886394136a",
+    "url": "./static/js/index.3792270c.chunk.js"
+  },
+  {
+    "revision": "53916a79fe868b1de377",
+    "url": "./static/js/layout.338c8b4f.chunk.js"
+  },
+  {
+    "revision": "22cd163c2398d898ed5e",
+    "url": "./static/js/login.e0664759.chunk.js"
+  },
+  {
+    "revision": "76c63db239f706d27ba6",
+    "url": "./static/js/main.03ff1d2f.chunk.js"
+  },
+  {
+    "revision": "87a93ef648694727be07",
+    "url": "./static/js/runtime-main.3ce32d38.js"
+  },
+  {
+    "revision": "f9731e460608881d8954",
+    "url": "./static/js/table-pages.a62ebc66.chunk.js"
+  },
+  {
+    "revision": "22d33c962bd8fdcf560aa0a3a21a90ac",
+    "url": "./static/media/bear.22d33c96.png"
+  },
+  {
+    "revision": "aa2cb723bdf4bbe3cbb187e0534d06f3",
+    "url": "./static/media/z-1.aa2cb723.gif"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.23eb7f7cee0d4d8fda39583731e09bc2.js"
+  "./precache-manifest.70d106d1ae301eb7939164ef191f14c6.js"
 );
 
 self.addEventListener('message', (event) => {
